23f9a7b08cf8ba7ec2e5cd8d8cbffad4efa8474c
Jeffy-Fung <92999261+Jeffy-Fung@users.noreply.github.com>
2024-08-04 12:52:29 +0800

    fix and validate min password length

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    password: { type: Number, required: true },
+    password: { type: String, required: true, minLength: 8 },
     email: { type: String, required: false, unique: true },
     phone: { type: Number, required: false },
     firstName: { type: String, required: false },
