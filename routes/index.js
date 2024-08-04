594c23caa6a40c90944294e6330503e2577f35d6
Jeffy-Fung <92999261+Jeffy-Fung@users.noreply.github.com>
2024-08-04 12:11:27 +0800

    parse JSON body for all api endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
-const RootRouter = require("express").Router();
+const express = require("express");
+
+const RootRouter = express.Router();
+
+RootRouter.use(express.json());
 
 RootRouter.use("/api", require("./api/index"));
 
