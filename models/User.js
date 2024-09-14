6d38484cfcf19a768fa2e733c3018e3d73ab1311
Jeffy-Fung <92999261+Jeffy-Fung@users.noreply.github.com>
2024-09-14 10:54:51 +0800

    feat: enforce more user model validation

--- models/User.js
+++ models/User.js
@@ -1,10 +1,33 @@
 const mongoose = require("mongoose");
 
+const noSpaces = (value) => {
+  if (/\s/.test(value)) {
+    throw new Error("No spaces are allowed.");
+  }
+};
+
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minLength: 8 },
-    email: { type: String, required: false, unique: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [noSpaces, "No spaces are allowed in the username."],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      validate: [noSpaces, "No spaces are allowed in the password."],
+    },
+    email: {
+      type: String,
+      required: false,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate: [noSpaces, "No spaces are allowed in the email."],
+    },
     phone: { type: Number, required: false },
     firstName: { type: String, required: false },
     lastName: { type: String, required: false },
