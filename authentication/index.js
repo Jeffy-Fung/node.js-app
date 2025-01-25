b051b8d71aedf5d2af36f6f7227aa8f939c3dd4f
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-01-25 15:14:46 +0800

    fix: failed to redirect due to missing session

--- authentication/index.js
+++ authentication/index.js
@@ -5,5 +5,4 @@ exports.authentication = passport.authenticate("jwt", { session: false });
 // TODO?: distinguish between request for token and request for exchanging user data with token
 exports.googleAuth = passport.authenticate("google", {
   scope: ["profile", "email"],
-  session: false,
 });
