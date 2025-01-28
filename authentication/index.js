062c19f66bb801abdb6ebea9e2bd20032d1869c2
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-01-28 23:18:37 +0800

    fix: google auth without session

--- authentication/index.js
+++ authentication/index.js
@@ -5,4 +5,5 @@ exports.authentication = passport.authenticate("jwt", { session: false });
 // TODO?: distinguish between request for token and request for exchanging user data with token
 exports.googleAuth = passport.authenticate("google", {
   scope: ["profile", "email"],
+  session: false,
 });
