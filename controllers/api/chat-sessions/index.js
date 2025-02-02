f23ac02c45091f37d9e33c4017587db4327b1329
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-02 22:19:54 +0800

    feat: create chat session endpoint

--- controllers/api/chat-sessions/index.js
+++ controllers/api/chat-sessions/index.js
@@ -7,6 +7,12 @@ exports.getChatSessions = async (req, res) => {
   return res.status(200).json({ data: chatSessions });
 };
 
+exports.createChatSession = async (req, res) => {
+  const user = req.user;
+  const chatSession = await ChatSession.create({ user: user._id });
+  return res.status(201).json({ data: chatSession });
+};
+
 exports.getChatHistories = async (req, res) => {
   const targetSessionId = req.params.id;
   const chatHistories = await ChatHistory.find({ session: targetSessionId });
--- routes/api/chat-sessions/index.js
+++ routes/api/chat-sessions/index.js
@@ -1,12 +1,14 @@
 const {
   getChatSessions,
   getChatHistories,
+  createChatSession,
 } = require("@controllers/api/chat-sessions");
 const { authentication } = require("@root/authentication");
 
 const ChatSessionRouter = require("express").Router();
 
 ChatSessionRouter.get("/", authentication, getChatSessions);
+ChatSessionRouter.post("/", authentication, createChatSession);
 ChatSessionRouter.get("/:id/chat-histories", authentication, getChatHistories);
 
 module.exports = ChatSessionRouter;
