1b2363a4a84fbec10d8b29a53824f52b800bdccb
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-03 21:08:02 +0800

    fix: serialise chat session related response

--- controllers/api/chat-sessions/index.js
+++ controllers/api/chat-sessions/index.js
@@ -4,17 +4,39 @@ const ChatHistory = require("@models/ChatHistory");
 exports.getChatSessions = async (req, res) => {
   const user = req.user;
   const chatSessions = await ChatSession.find({ user: user._id });
-  return res.status(200).json({ data: chatSessions });
+  return res.status(200).json({
+    data: chatSessions.map((chatSession) => ({
+      id: chatSession._id,
+      userId: chatSession.user,
+      createdAt: chatSession.createdAt,
+      updatedAt: chatSession.updatedAt,
+    })),
+  });
 };
 
 exports.createChatSession = async (req, res) => {
   const user = req.user;
   const chatSession = await ChatSession.create({ user: user._id });
-  return res.status(201).json({ data: chatSession });
+  return res.status(201).json({
+    data: {
+      id: chatSession._id,
+      userId: user._id,
+      createdAt: chatSession.createdAt,
+      updatedAt: chatSession.updatedAt,
+    },
+  });
 };
 
 exports.getChatHistories = async (req, res) => {
   const targetSessionId = req.params.id;
   const chatHistories = await ChatHistory.find({ session: targetSessionId });
-  return res.status(200).json({ data: chatHistories });
+  return res.status(200).json({
+    data: chatHistories.map((chatHistory) => ({
+      id: chatHistory._id,
+      role: chatHistory.role === "human" ? "user" : chatHistories.role,
+      message: chatHistory.message,
+      createdAt: chatHistory.createdAt,
+      updatedAt: chatHistory.updatedAt,
+    })),
+  });
 };
