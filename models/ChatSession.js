7c90705f2149e27ea7c5d981f8d2e4caf4f5280c
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-08 20:08:49 +0800

    feat: introduce enum session type

--- models/ChatSession.js
+++ models/ChatSession.js
@@ -4,6 +4,7 @@ const chatSessionSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     chatHistories: [{ type: mongoose.Schema.Types.ObjectId, ref: "ChatHistory" }],
+    type: { type: String, enum: ["simple", "rag"] },
   },
   { timestamps: true }
 );
