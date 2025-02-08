65839c8859561ef9a1b5f0abda42e4aff248e453
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-08 18:10:32 +0800

    feat: rag chat route

--- routes/api/chats/index.js
+++ routes/api/chats/index.js
@@ -1,9 +1,11 @@
 const {
   createChat,
+  createRagChat,
 } = require("@controllers/api/chats");
 
 const ChatRouter = require("express").Router();
 
 ChatRouter.post("/", createChat);
+ChatRouter.post("/rag", createRagChat);
 
 module.exports = ChatRouter;
