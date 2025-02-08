c49e819e0f69e97811a13db96cd102499d766bd1
Jeffy-Fung <jeffyfung0414@gmail.com>
2025-02-09 00:15:53 +0800

    refactor: pass in target news article to talk with

--- controllers/api/chats/index.js
+++ controllers/api/chats/index.js
@@ -1,6 +1,5 @@
 const { simpleChat, ragChat } = require("@services/ai-app");
 const ChatHistory = require("@models/ChatHistory");
-const News = require("@models/News");
 
 exports.createChat = async (req, res) => {
   const { messages, sessionId } = req.body;
@@ -34,7 +33,7 @@ exports.createChat = async (req, res) => {
 };
 
 exports.createRagChat = async (req, res) => {
-  const { messages, sessionId } = req.body;
+  const { messages, sessionId, newsArticleIds } = req.body;
   const inputUserMessage = messages[0].text;
 
   // TODO: User data base transation
@@ -51,13 +50,7 @@ exports.createRagChat = async (req, res) => {
     chatHistory.message,
   ]);
 
-  const recentNews = await News.find({
-    publishedAt: { $gte: new Date(Date.now() - 1000 * 60 * 60 * 48) },
-  });
-
-  const filtered_document_ids = recentNews.map((news) => news._id);
-
-  const ragChatResponse = await ragChat(serializedMessages, filtered_document_ids, inputUserMessage);
+  const ragChatResponse = await ragChat(serializedMessages, newsArticleIds, inputUserMessage);
 
   const aiMessage = ragChatResponse.output.content;
 
