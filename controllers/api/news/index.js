dbc572fe760451537475af53247edbde71d0c68c
Jeffy-Fung <92999261+Jeffy-Fung@users.noreply.github.com>
2025-02-12 10:53:45 +0800

    fix: memory issue when too much news are crawled at once

--- controllers/api/news/index.js
+++ controllers/api/news/index.js
@@ -4,16 +4,31 @@ const News = require("../../../models/News");
 
 exports.crawlLatest = async (req, res) => {
   const latestNews = await fetchLatestNews();
-  const newsContent = await crawlNews(latestNews.map((news) => news.url));
 
-  const newsWithContent = latestNews.map((news) => ({
-    title: news.title,
-    description: news.description,
-    url: news.url,
-    content: newsContent.filter((item) => item.url === news.url)[0].content,
-    source: news.source.name,
-    publishedAt: news.publishedAt,
-  }));
+  const newsBySource = latestNews.reduce((acc, news) => {
+    if (!acc[news.source.name]) {
+      acc[news.source.name] = [];
+    }
+    acc[news.source.name].push(news.url);
+    return acc;
+  }, {});
+
+  const newsWithContent = [];
+
+  try {
+    for (const source in newsBySource) {
+      const newsUrls = newsBySource[source];
+      const newsContent = await crawlNews(newsUrls);
+
+      newsWithContent.push(...newsUrls.map((url) => ({
+        title: latestNews.find(news => news.url === url).title,
+        description: latestNews.find(news => news.url === url).description,
+        url: url,
+        content: newsContent.find(item => item.url === url).content,
+        source: source,
+        publishedAt: latestNews.find(news => news.url === url).publishedAt,
+      })));
+    }
 
   try {
     newsWithContent.forEach(async (news) => {
